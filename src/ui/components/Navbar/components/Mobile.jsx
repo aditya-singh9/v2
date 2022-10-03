b270b4c8a5e24e2075c58e0f004180293fd15c84
Udoka <udochukwukaonyela@gmail.com>
2022-10-03 23:48:54 +0100

    added contact link for mobile state

--- src/ui/components/Navbar/components/Mobile.jsx
+++ src/ui/components/Navbar/components/Mobile.jsx
@@ -15,6 +15,10 @@ export default function Mobile(props) {
       href: "/awards",
     },
     {
+      label: "/contact",
+      href: "/contact"
+    },
+    {
       label: "/stack",
       href: "/stack",
     },
