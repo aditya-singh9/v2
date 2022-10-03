bc09c0f23622c5e24e128f3064b04a7abedb95b8
Udoka <udochukwukaonyela@gmail.com>
2022-10-03 16:35:15 +0100

    added to new directory

---
+++ src/pages/contact/components/Contact.jsx
@@ -0,0 +1,18 @@
+export default function Contact (props) {
+    return (
+        <div className="flex  items-center w-fit gap-4">
+            <img
+                src={props.iconPath}
+                alt="icon"
+                className="h-6 select-none"
+                draggable="false"
+            />
+            {!props.link
+                ?
+                (<p className="text-sm">{props.text}</p>)
+                :
+                (<a href={props.link} target="_blank" className="text-sm cursor-pointer">{props.text}</a>)
+            }
+        </div>
+    );
+}
