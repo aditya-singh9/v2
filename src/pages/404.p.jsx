3dfecbd515410c408c0dcccaebf56c88dd38b9be
Udoka <udochukwukaonyela@gmail.com>
2022-10-03 23:51:16 +0100

    remove repetitive classes

--- src/pages/404.p.jsx
+++ src/pages/404.p.jsx
@@ -5,7 +5,7 @@ export default function FourZeroFour() {
     <div class="flex bg-black h-screen">
       <div class="container mx-auto flex items-center">
         <div class="w-full flex flex-col items-center space-y-5 transition duration-300">
-          <h1 class="font-extrabold text-5xl text-center text-center text-white text-opacity-90">
+          <h1 class="font-extrabold text-5xl text-center text-white text-opacity-90">
             404: Page not found
           </h1>
           <Link href="/">
