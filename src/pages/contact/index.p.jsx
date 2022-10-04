57928320e279d7801ccdfc99ebd403656b9d1dcd
Udoka <udochukwukaonyela@gmail.com>
2022-10-04 06:23:52 +0100

    changed email address

--- src/pages/contact/index.p.jsx
+++ src/pages/contact/index.p.jsx
@@ -16,22 +16,6 @@ export default function Contacts(props) {
   const [message, setMessage] = useState("");
   const [messageError, setMessageError] = useState(false);
   const [loading, setLoading] = useState(false);
-  const contactObj = [
-    {
-      iconPath: "/icons/phone.svg",
-      text: "+1 (480) 555-0103", // change this to your phone number
-      link: "tel:+1 (480) 555-0103", // replace this with your phone number
-    },
-    {
-      iconPath: "icons/email.svg",
-      text: "alma.lawson@example.com", // change this to your email
-      link: "mailto:alma.lawson@example.com", // replace this with your email
-    },
-    {
-      iconPath: "icons/location.svg",
-      text: "1901 Thornridge Cir. Shiloh, Hawaii 81063", // change this to your address
-    },
-  ];
   function handleSubmit() {
     setLoading(true);
     if (!isString(name) || !isEmail(email) || !isString(message)) {
@@ -51,7 +35,7 @@ export default function Contacts(props) {
       return 
     }
     return sendMessage({
-      my_email: "udochukwukaonyela@gmail.com", // Change this to your email address
+      my_email: "aditya0930singh@gmail.com",
       user_name: name,
       user_email: email,
       message: message
@@ -66,25 +50,17 @@ export default function Contacts(props) {
         <meta property="og:title" content="Projects • Aditya Singh" />
         <meta property="twitter:title" content="Projects • Aditya Singh" />
       </Head>
-      <section className="flex items-center text-white w-full lg:flex-row flex-col gap-12">
+      <section className="flex items-center text-white w-full flex-col gap-12">
         <div className="flex flex-col items-start lg:w-full w-11/12 justify-start space-y-4 basis-50% gap-12 max-h-screen">
           <div className="flex items-center space-x-2 tracking-[-0.02rem]">
-            <img
-              src="/icons/email.svg"
-              className="h-[40px] select-none"
-              draggable="false"
-            />
             <p className="font-bold text-4xl text-opacity-80 ">Contact Me</p>
           </div>
-          {contactObj.map((contact, index) => (
-            <Contact key={"contact-" + index} {...contact} index={index} />
-          ))}
         </div>
         <motion.div
           initial={{ y: 10, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           transition={{ duration: 1, delay: 0.7 }}
-          className="flex flex-col items-start justify-start gap-6 basis-50% lg:w-3/4 w-11/12 bg-[#1E222F] border-2 border-white/25 lg:px-12 px-6 py-8 rounded-md"
+          className="flex flex-col items-start justify-start gap-6 basis-50% lg:w-full w-11/12 bg-[#1E222F] border-2 border-white/25 lg:px-12 px-6 py-8 rounded-md"
         >
           <p className="w-full">Fill the form and i'll get back to you</p>
           <Form
